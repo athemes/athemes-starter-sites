fb351a46a9c70ce42923446eb785f49f65ffeb99
Rodrigo Teixeira <maaprote@gmail.com>
2024-03-27 10:08:16 -0300

    Bump version

--- athemes-starter-sites.php
+++ athemes-starter-sites.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       aThemes Starter Sites
  * Description:       Starter Sites for Sydney and Botiga
- * Version:           1.0.50
+ * Version:           1.0.51
  * Author:            aThemes
  * Author URI:        https://athemes.com
  * License:           GPL-2.0+
